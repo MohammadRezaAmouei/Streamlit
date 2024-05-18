3ac767691325ff55a6e23ba36dbd305d93311099
Mohammad Reza Amouei <mohammadrezaamouei07@gmail.com>
2024-05-18 15:50:42 +0330

    change python file

--- remove_bg.py
+++ remove_bg.py
@@ -3,9 +3,23 @@ from rembg import remove
 from PIL import Image
 import io
 import time
+import base64
 
-st.header("Background Remover")
-input_img = st.file_uploader(label="Upload your image", type=["png", "jpg", "jpeg", "webp"])
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("style.css")
+
+def get_image_download_link(img, filename, text):
+    buffered = io.BytesIO()
+    img.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+    href = f'<a href="data:file/png;base64,{img_str}" download="{filename}">{text}</a>'
+    return href
+
+st.markdown("<h1>Background Remover</h1>", unsafe_allow_html=True)
+input_img = st.file_uploader(label="", type=["png", "jpg", "jpeg", "webp"])
 
 if input_img is not None:
     img_bytes = input_img.read()
@@ -27,5 +41,6 @@ if input_img is not None:
     with col2:
         st.subheader("Background Removed")
         st.image(processed_image, use_column_width=True)
+        st.markdown(get_image_download_link(processed_image, "processed_image.png", "Download Processed Image"), unsafe_allow_html=True)
 
     progress_bar.empty()
